d752147af1076555a393c89dc941b8f681a0aa0a
Alejo Boga <alejoboga20@gmail.com>
2022-07-28 12:42:46 -0300

    Get Car by Id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda'];
+    return this.cars;
+  }
+
+  @Get('/:id')
+  gerCarById(@Param('id') id: string) {
+    return { car: this.cars[+id] };
   }
 }
